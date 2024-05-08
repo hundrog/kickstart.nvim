900b97d97b141c43b32fe7dda9b96fc6c5199b5e
Oscar Recio <reciog.oscar@hotmail.com>
2024-05-08 11:55:07 -0600

    fix add vim-tmux-navigator l key

--- lua/custom/plugins/vim-tmux-navigator.lua
+++ lua/custom/plugins/vim-tmux-navigator.lua
@@ -17,5 +17,5 @@ return {
   vim.keymap.set('n', 'C-h', ':TmuxNavigateLeft<CR>'),
   vim.keymap.set('n', 'C-k', ':TmuxNavigateUp<CR>'),
   vim.keymap.set('n', 'C-j', ':TmuxNavigateDown<CR>'),
-  vim.keymap.set('n', 'C-h', ':TmuxNavigateRight<CR>'),
+  vim.keymap.set('n', 'C-l', ':TmuxNavigateRight<CR>'),
 }
